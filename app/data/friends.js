d4d92d0e5208fdd10b66eb1323415d9555357428
Renato <zamudiorenato@gmail.com>
2018-02-01 13:36:39 -0500

    Dawg Finder

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+let dawgs = [
+  {
+    name: "Bree",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAhttp://www.jasonmillstein.com/wp-content/uploads/Dog-Portrait-Shepherd-Pet-Photography-Phoenix-Arizona-Millstein.jpg-240x240.jpgAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Karl",
+    photo: "https://pbs.twimg.com/prohttps://t4.ftcdn.net/jpg/01/40/42/33/240_F_140423360_K2QVsvTFy8EpXtyGjJxYpIUFtZpCe7Zp.jpgfile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Benji",
+    photo: "https://t4.ftcdn.net/jpg/01/66/09/51/240_F_166095165_KWEHeJHiRGalTz9fcK5H3z0psiT5an0l.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Darby",
+    photo: "http://mmphotos.com.au/wp/wp-content/uploads/2014/11/Dog-Portrait-Photography-Studio-020-240x240.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Lance",
+    photo: "https://static1.squarespace.com/static/54f915d8e4b0d13f30b8cb91/t/58954ba01e5b6cc3eb6282dc/1486096641470/",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Finch",
+    photo: "https://i.pinimg.com/736x/95/ec/1f/95ec1fa4212407411b29f35ffadb8eb6--puppy-dog-eyes-animal-shelters.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = dawgs;
